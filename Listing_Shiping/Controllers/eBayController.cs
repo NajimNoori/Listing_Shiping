179e8caed4a2cfdc4e6d2fa2f19ee0ea09d1ba8c
NajimNoori <najimnoori@gmail.com>
2020-01-12 21:51:50 -0600

    bootstrap 4.4.1 added

---
+++ Listing_Shiping/Controllers/eBayController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Listing_Shiping.Controllers
+{
+    public class eBayController : Controller
+    {
+        // GET: eBay
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EbayFind()
+        {
+            return View();
+        }
+    }
+}
